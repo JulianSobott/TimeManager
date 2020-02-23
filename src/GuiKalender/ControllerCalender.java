c3c8598c2f8f7760cb2182ede8e03f1477a98c23
Lukas Mendel <lukasmendel14043@gmail.com>
2020-02-23 18:46:09 +0100

    start implementing dynamic GridPane

--- src/GuiKalender/ControllerCalender.java
+++ src/GuiKalender/ControllerCalender.java
@@ -1,4 +1,44 @@
 package GuiKalender;
 
-public class ControllerCalender {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerCalender implements Initializable {
+
+    @FXML
+    private GridPane GridPaneTimetable;
+
+    private double CELL_PERCENTAGE_WIDTH = 25;
+    private double playgroundSize;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+
+    private void generateGridPane(GridPane gridPane) {
+
+        CELL_PERCENTAGE_WIDTH = 100 / playgroundSize;
+
+        for (int i = 0; i < playgroundSize; i++) {
+            ColumnConstraints col = new ColumnConstraints();
+            col.setPercentWidth(CELL_PERCENTAGE_WIDTH);
+            gridPane.getColumnConstraints().add(col);
+        }
+
+        for (int i = 0; i < playgroundSize; i++) {
+            RowConstraints row = new RowConstraints();
+            row.setPercentHeight(CELL_PERCENTAGE_WIDTH);
+            gridPane.getRowConstraints().add(row);
+        }
+
+    }
 }
