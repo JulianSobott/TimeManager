a3f768f6d98dfdd9ce27c6a89226f50a6d5146f8
Lukas Mendel <lukasmendel14043@gmail.com>
2020-02-24 10:25:57 +0100

    add Classes to calendar Tap

---
+++ src/GuiKalender/Lesson.java
@@ -0,0 +1,10 @@
+package GuiKalender;
+
+import javafx.scene.layout.VBox;
+
+public class Lesson extends VBox {
+
+        private String building;
+        private String roomNr;
+
+}
---
+++ src/GuiKalender/Subject.java
@@ -0,0 +1,12 @@
+package GuiKalender;
+
+import java.util.ArrayList;
+
+public class Subject {
+
+    private ArrayList<Lesson> lessons = new ArrayList<>();
+    private String color;
+    private String professor;
+
+
+}
