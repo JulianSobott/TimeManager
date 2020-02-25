8a7303e807efbfb35f0595e886b8965393b8862e
Lukas Mendel <lukasmendel14043@gmail.com>
2020-02-25 17:58:16 +0100

    [Feature] update Design Context Menu and Tabs

--- src/Calendar/Gui/NewLesson/ControllerLesson.java
+++ src/Calendar/Gui/NewLesson/ControllerLesson.java
@@ -1,13 +1,19 @@
 package Calendar.Gui.NewLesson;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerLesson implements Initializable {
 
+    @FXML
+    private Button buttonSave;
 
+    @FXML
+    private Button buttonCancel;
 
 
 
@@ -15,6 +21,7 @@ public class ControllerLesson implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
 
 
     }
