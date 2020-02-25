f35e14477077f615c3ac23bac83748fe2fcab263
Lukas Mendel <lukasmendel14043@gmail.com>
2020-02-25 19:42:08 +0100

    [FIX] issue with loading a new Window

--- src/Calendar/Gui/NewLesson/ControllerLesson.java
+++ src/Calendar/Gui/NewLesson/ControllerLesson.java
@@ -2,7 +2,7 @@ package Calendar.Gui.NewLesson;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,14 +13,44 @@ public class ControllerLesson implements Initializable {
     private Button buttonSave;
 
     @FXML
-    private Button buttonCancel;
+    private Button buttonClose;
 
+    @FXML
+    private ListView listViewSubjects;
+
+    @FXML
+    private CheckBox checkBoxTutorial;
+
+    @FXML
+    private CheckBox checkBoxDoubleHour;
+
+    @FXML
+    private CheckBox checkBoxInterval;
+
+    @FXML
+    private TextField textFieldProfessor;
+
+    @FXML
+    private TextField textFieldSubject;
+
+    @FXML
+    private TextField textFieldCourseLocation;
+
+    @FXML
+    private ColorPicker colorPickerSubjectColor;
+
+    @FXML
+    private Button buttonAddSubject;
+
+    @FXML
+    private Button buttonDeleteSubject;
 
 
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
 
 
 
