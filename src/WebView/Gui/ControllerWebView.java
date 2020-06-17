eda39c70695e2039083c6895ae910abf77afd776
Lukas Mendel <lukasmendel14043@gmail.com>
2020-06-17 19:31:33 +0200

    update some features

--- src/WebView/Gui/ControllerWebView.java
+++ src/WebView/Gui/ControllerWebView.java
@@ -4,14 +4,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.geometry.Side;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -32,12 +31,12 @@ public class ControllerWebView implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         mainWebViewVBox.setId("BoxWebView");
-        createMainWebViewBoxContextMenu();
+        createContextMenu(mainWebViewVBox, 900.00, 900.00);
 
     }
 
 
-    private HBox createNewHBox(String id, int height) {
+    private HBox createNewHBox(String id, Double height) {
 
         HBox hBox = new HBox();
         hBox.setPrefHeight(height);
@@ -61,12 +60,12 @@ public class ControllerWebView implements Initializable {
      */
 
 
-    private void createMainWebViewBoxContextMenu() {
+    private void createContextMenu(Pane pane , Double x, Double y) {
 
-        ContextMenu contextMenuCalendar = new ContextMenu();
+        ContextMenu contextMenu = new ContextMenu();
 
         MenuItem menuItemSplitHorizontal = new MenuItem("Horizontal teilen");
-        generateEventSplitHorizontal(menuItemSplitHorizontal);
+        generateEventSplitHorizontal(menuItemSplitHorizontal, pane);
 
         MenuItem menuItemSplitVertical = new MenuItem("Vertikal teilen");
         generateEventSplitVertical(menuItemSplitVertical);
@@ -75,42 +74,50 @@ public class ControllerWebView implements Initializable {
 
         MenuItem menuItemDeleteEntry = new MenuItem("Eintrag löschen");
 
-        contextMenuCalendar.getItems().addAll(menuItemSplitHorizontal, menuItemSplitVertical, menuItemEditURL, menuItemDeleteEntry);
-        anchorPaneWebview.setOnMouseClicked(event ->
-                contextMenuCalendar.show(anchorPaneWebview, Side.BOTTOM, anchorPaneWebview.getWidth() / 2, -anchorPaneWebview.getHeight() / 2));
+        contextMenu.getItems().addAll(menuItemSplitHorizontal, menuItemSplitVertical, menuItemEditURL, menuItemDeleteEntry);
+        pane.setOnMouseClicked(event ->
+                contextMenu.show(pane, Side.BOTTOM, x / 2 , -y / 2));
 
         //breite -> rechts / höhe -> oben
     }
 
 
-    private void generateEventSplitHorizontal(MenuItem menuItemSplitHorizontal) {
+    private void generateEventSplitHorizontal(MenuItem menuItemSplitHorizontal, Pane pane) {
 
         menuItemSplitHorizontal.setOnAction(actionEvent -> {
 
-                HBox h1 = createNewHBox("BoxWebViewLight", 300);
+                pane.setOnMouseClicked(null);
+
+                HBox h1 = createNewHBox("BoxWebViewLight", pane.getHeight() * 0.45);
                 h1.getChildren().add(generateAddURL());
+                createContextMenu(h1, pane.getWidth() /2 , pane.getHeight() /2);
 
-
-                HBox h2 = createNewHBox("BoxWebViewLight", 300);
+                HBox h2 = createNewHBox("BoxWebViewLight", pane.getHeight() * 0.45);
                 h2.getChildren().add(generateAddURL());
-
+                createContextMenu(h2, pane.getWidth() /2 , pane.getHeight() /2);
 
                 mainWebViewVBox.getChildren().addAll(h1, h2);
+
 
         });
 
     }
 
 
-    private HBox generateAddURL(){
+    private Pane generateAddURL(){
 
         HBox hBox = new HBox();
-        hBox.setSpacing(10);
-        Button button = new Button("URL anzeigen");
-        TextField textField = new TextField();
-        textField.setPromptText("http://google.de");
 
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(10);
+
+        Button button = new Button("URL anzeigen");
+        //Fehlendes Event zum Laden der Webseite
+
+        TextField textField = new TextField();
+        textField.setPromptText("http://www.google.de");
         hBox.getChildren().addAll(textField, button);
+
         return hBox;
     }
 
