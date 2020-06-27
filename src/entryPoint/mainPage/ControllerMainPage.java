c60622b0a36b1d2715cd1d3802ea0aa0bf2aaba2
Julian Sobott <julian.sobott@gmail.com>
2020-06-27 04:20:32 +0200

    Can Install tabs

--- src/entryPoint/mainPage/ControllerMainPage.java
+++ src/entryPoint/mainPage/ControllerMainPage.java
@@ -1,16 +1,68 @@
 package entryPoint.mainPage;
 
 import GuiElements.TabCustom;
+import GuiElements.TabSettings;
 import GuiElements.TabWindow;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
+import tabs.InstalledTabs;
+import tabs.Tabs;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class ControllerMainPage {
+public class ControllerMainPage implements Initializable{
 
+    @FXML TabWindow tabWindow;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        List<Tabs.TabWrapper> tabs = InstalledTabs.get().getTabs();
+        addTabs(tabs);
+        InstalledTabs.get().addListener(change -> {
+            while (change.next()) {
+                if(change.wasPermutated()) {
+                    for(int i = change.getFrom(); i < change.getTo(); i++) {
+                        // TODO: permutate
+                    }
+                }
+                else if(change.wasUpdated()) {
+                    // TODO: update
+                }
+                else {
+                    removeTabs(change.getRemoved());
+                    addTabs(change.getAddedSubList());
+                }
+            }
+        });
+    }
+
+    private void removeTabs(List<? extends Tabs.TabWrapper> removed) {
+        for (Tabs.TabWrapper tab: removed) {
+            // TODO: Only works as long, as tab has only name as text
+            tabWindow.getTabs().removeIf(tab1 -> tab1.getText().equals(tab.tabData.name));
+        }
+    }
+
+    private void addTabs(List<? extends Tabs.TabWrapper> tabs) {
+        for(Tabs.TabWrapper tab : tabs) {
+            // TODO: Allow reorder tabs
+            // append before + (add) tab
+            tabWindow.getTabs().add(tabWindow.getTabs().size() - 1, newTab(tab));
+        }
+    }
+
+    private Tab newTab(Tabs.TabWrapper tab) {
+        TabCustom tabWidget = new TabCustom();
+        tabWidget.setContent(tab.fxmlMain);
+        tabWidget.setText(tab.tabData.name);
+        TabSettings settings = new TabSettings();
+        settings.setContent(tab.fxmlSettings);
+        tabWidget.setSettingsNode(settings);
+        return tabWidget;
+    }
 }
