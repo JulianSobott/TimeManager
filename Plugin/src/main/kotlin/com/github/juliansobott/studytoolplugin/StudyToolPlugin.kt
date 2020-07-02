dfa65dced933b12a4868f81500734aaea6d09db8
Julian Sobott <julian.sobott@gmail.com>
2020-07-02 12:09:20 +0200

    Minimal structure for tasks

--- Plugin/src/main/kotlin/com/github/juliansobott/studytoolplugin/StudyToolPlugin.kt
+++ Plugin/src/main/kotlin/com/github/juliansobott/studytoolplugin/StudyToolPlugin.kt
@@ -1,7 +1,23 @@
 package com.github.juliansobott.studytoolplugin
 
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskAction
+
+/**
+ * Tasks:
+ * - publish plugin
+ * - Check plugin
+ * - test plugin installed
+ * - test plugin not installed
+ */
+
+const val GROUP = "study tool"
+
+const val TASK_PUBLISH = "publish"
+const val TASK_CHECK = "check"
+const val TASK_TEST = "test"
 
 class StudyToolPlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -10,5 +26,45 @@ class StudyToolPlugin : Plugin<Project> {
                 println("Hello from Launch")
             }
         }
+        // Register tasks
+        project.tasks.register<PublishTask>(TASK_PUBLISH, PublishTask::class.java)
+        project.tasks.register<CheckTask>(TASK_CHECK, CheckTask::class.java)
+        project.tasks.register<TestPluginTask>(TASK_TEST, TestPluginTask::class.java)
+    }
+}
+
+open class CheckTask : DefaultTask() {
+
+    init {
+        group = GROUP
+    }
+    @TaskAction
+    fun checkPlugin() {
+        println("Hello: Checking!")
+    }
+}
+
+open class TestPluginTask : DefaultTask() {
+
+    init {
+        group = GROUP
+    }
+
+    @TaskAction
+    fun testPlugin() {
+        println("Hello: testing!")
+    }
+}
+
+open class PublishTask : DefaultTask() {
+
+    init {
+        group = GROUP
+        dependsOn(TASK_CHECK)
+    }
+
+    @TaskAction
+    fun publishStudyPlugin() {
+        println("Hello: Publishing!")
     }
 }
