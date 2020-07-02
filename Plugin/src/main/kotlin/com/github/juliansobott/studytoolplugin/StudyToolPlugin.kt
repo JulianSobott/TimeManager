b0d1f42df00732cda9e4e42f03895b501bde36f0
Julian Sobott <julian.sobott@gmail.com>
2020-07-02 14:53:20 +0200

    Add example project and start buildPublish task

--- Plugin/src/main/kotlin/com/github/juliansobott/studytoolplugin/StudyToolPlugin.kt
+++ Plugin/src/main/kotlin/com/github/juliansobott/studytoolplugin/StudyToolPlugin.kt
@@ -1,9 +1,15 @@
 package com.github.juliansobott.studytoolplugin
 
+import org.gradle.api.Action
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.FileCollection
+import org.gradle.api.internal.file.copy.CopyAction
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.bundling.Zip
+import java.io.File
+import java.io.FileNotFoundException
 
 /**
  * Tasks:
@@ -15,21 +21,25 @@ import org.gradle.api.tasks.TaskAction
 
 const val GROUP = "study tool"
 
-const val TASK_PUBLISH = "publish"
-const val TASK_CHECK = "check"
-const val TASK_TEST = "test"
+const val TASK_PUBLISH = "publishPlugin"
+const val TASK_BUILD_PUBLISH = "buildPublish"
+const val TASK_CHECK = "checkPlugin"
+const val TASK_TEST = "testPlugin"
 
 class StudyToolPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        project.task("helloLaunch") {
-            it.doLast {
-                println("Hello from Launch")
-            }
-        }
+        // Extension
+        project.extensions.create<StudyToolExtension>("StudyTool", StudyToolExtension::class.java)
+
         // Register tasks
+        project.tasks.register<BuildPublishTask>(TASK_BUILD_PUBLISH, BuildPublishTask::class.java)
         project.tasks.register<PublishTask>(TASK_PUBLISH, PublishTask::class.java)
         project.tasks.register<CheckTask>(TASK_CHECK, CheckTask::class.java)
         project.tasks.register<TestPluginTask>(TASK_TEST, TestPluginTask::class.java)
+
+        // Dependencies
+        project.tasks.getByPath(TASK_BUILD_PUBLISH).dependsOn("clean", "jar", TASK_CHECK)
+        project.tasks.getByPath(TASK_PUBLISH).dependsOn(TASK_BUILD_PUBLISH)
     }
 }
 
@@ -60,11 +70,49 @@ open class PublishTask : DefaultTask() {
 
     init {
         group = GROUP
-        dependsOn(TASK_CHECK)
+
     }
 
     @TaskAction
     fun publishStudyPlugin() {
         println("Hello: Publishing!")
     }
+}
+
+
+open class BuildPublishTask : Zip() {
+
+    var infoPath: String = ""
+
+    init {
+        group = GROUP
+
+        archiveBaseName.set("publish")
+        destinationDirectory.set(File("build/"))
+        from(File("build/libs"))
+
+        doFirst {
+            check()
+        }
+    }
+
+    private fun check() {
+        val ext = project.extensions.getByType(StudyToolExtension::class.java)
+        println(ext.infoPath)
+        infoPath = ext.infoPath
+
+        println("Info Path: $infoPath")
+
+        from(File("build/libs"))
+        val infoFiles = File(infoPath)
+        into("info").from(infoFiles)
+        if (!infoFiles.exists()) {
+            throw FileNotFoundException("Info files not found: $infoPath")
+        }
+    }
+}
+
+
+open class StudyToolExtension {
+    var infoPath: String = "info"
 }
---
+++ Plugin/src/main/kotlin/com/github/juliansobott/studytoolplugin/Tes.java
@@ -0,0 +1,4 @@
+package com.github.juliansobott.studytoolplugin;
+
+public class Tes {
+}
